6a4b9e98bdfd96e620ce5ed46bc700598f2d287a
AlanoudBinSalamah <alanoud.binsalamah@gmail.com>
2019-01-09 06:51:45 +0300

    homework done

---
+++ weather-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Search from './Components/Search';
+
+class App extends Component {
+  render(){
+return(
+<Search/>
+);
+  }
+    
+  }
+
+
+export default App;
